2593fe5d820bb049d06b89bec18dce5434ee68cc
Batson Seales <batson.seales@gmail.com>
2023-06-14 12:40:29 -0500

    Set up NetworkManager.

---
+++ SeanAllen_Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  SeanAllen_Appetizers
+//
+//  Created by Batson Seales on 6/14/23.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidUrl
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
