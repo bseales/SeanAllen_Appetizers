ca6c82edc4a3d5bfe1e6061b0706b5c63ef87d1b
Batson Seales <batson.seales@gmail.com>
2023-06-13 19:52:12 -0500

    Initial Commit

---
+++ SeanAllen_Appetizers/SeanAllen_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeanAllen_AppetizersApp.swift
+//  SeanAllen_Appetizers
+//
+//  Created by Batson Seales on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SeanAllen_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
