2769a839a0cfd52b22a51b9806c517e38d156b2d
Batson Seales <batson.seales@gmail.com>
2023-06-15 18:29:13 -0500

    Images are now fetched from the network.

--- SeanAllen_Appetizers/NetworkManager.swift
+++ SeanAllen_Appetizers/NetworkManager.swift
@@ -5,13 +5,13 @@
 //  Created by Batson Seales on 6/14/23.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
-    
     static let baseUrl = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerUrl = baseUrl + "appetizers"
+    private let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
@@ -44,6 +44,32 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        
+        task.resume()
+    }
+    
+    func downloadImage(fromUrlString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         
         task.resume()
