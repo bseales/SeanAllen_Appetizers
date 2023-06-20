ef142aec52b6f4751ca3d910bfd7117c308dfe5f
Batson Seales <batson.seales@gmail.com>
2023-06-19 22:39:38 -0500

    Updated file reference for String extension file.

---
+++ SeanAllen_Appetizers/Utilities/Extensions/String+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  String+Ext.swift
+//  SeanAllen_Appetizers
+//
+//  Created by Batson Seales on 6/19/23.
+//
+
+import Foundation
+
+extension String {
+    
+    var isValidEmail: Bool {
+        let emailFormat         = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate      = NSPredicate(format: "SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: self)
+    }
+}
