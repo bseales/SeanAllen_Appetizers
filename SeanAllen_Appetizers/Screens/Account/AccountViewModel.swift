7441358df9a6299c3b0c0bc730a6494e211bb264
Batson Seales <batson.seales@gmail.com>
2023-06-19 22:02:24 -0500

    Added AccountViewModel with form checks and saveChanges basic implementation.

---
+++ SeanAllen_Appetizers/Screens/Account/AccountViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  AccountViewModel.swift
+//  SeanAllen_Appetizers
+//
+//  Created by Batson Seales on 6/19/23.
+//
+
+import SwiftUI
+
+final class AccountViewModel: ObservableObject {
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var email = ""
+    @Published var birthdate = Date()
+    @Published var extraNapkins = false
+    @Published var frequentRefills = false
+    @Published var alertItem: AlertItem? 
+    
+    var isValidForm: Bool {
+        guard !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty else {
+            alertItem = AlertContext.invalidForm
+            return false
+        }
+        
+        guard email.isValidEmail else {
+            alertItem = AlertContext.invalidEmail
+            return false
+        }
+        
+        return true
+    }
+    
+    func saveChanges() {
+        guard isValidForm else { return }
+        
+        print("saved changes.")
+    }
+}
