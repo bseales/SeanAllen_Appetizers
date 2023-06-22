96cdfeff624079c549aa675e550a3b1cc6af2265
Batson Seales <batson.seales@gmail.com>
2023-06-22 17:39:59 -0500

    FocusState for Account screen.

--- SeanAllen_Appetizers/Screens/Account/AccountView.swift
+++ SeanAllen_Appetizers/Screens/Account/AccountView.swift
@@ -9,14 +9,30 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject var viewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit { focusedTextField = .lastName }
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit { focusedTextField = .email }
+                        .submitLabel(.next)
+                    
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit { focusedTextField = nil }
+                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .autocorrectionDisabled(true)
                         .textInputAutocapitalization(.never)
@@ -38,6 +54,13 @@ struct AccountView: View {
                     Text("Requests")
                 }
             }
+            .toolbar(content: {
+                ToolbarItem(placement: .keyboard) {
+                    Button("Dismiss") {
+                        focusedTextField = nil
+                    }
+                }
+            })
             .navigationTitle(Text("🐶 Account"))
         }
         .onAppear {
