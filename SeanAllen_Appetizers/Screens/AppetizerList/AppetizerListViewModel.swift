6b56627b2d168641ce63924d8991f5d9c509fbcc
Batson Seales <batson.seales@gmail.com>
2023-06-22 18:15:57 -0500

    Updated networking to use iOS 15 async features.

--- SeanAllen_Appetizers/Screens/AppetizerList/AppetizerListViewModel.swift
+++ SeanAllen_Appetizers/Screens/AppetizerList/AppetizerListViewModel.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-final class AppetizerListViewModel: ObservableObject {
+@MainActor final class AppetizerListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
     @Published var alertItem: AlertItem?
     @Published var isLoading = false
@@ -17,35 +17,59 @@ final class AppetizerListViewModel: ObservableObject {
     func getAppetizers() {
         isLoading = true
         
-        NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async { [weak self] in
-                guard let self = self else {
-                    return
-                }
-                
+        Task {
+            do {
+                appetizers = try await NetworkManager.shared.getAppetizers()
                 isLoading = false
-                
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                    
-                case .failure(let error):
-                    switch error {
+            } catch {
+                if let apError = error as? APError {
+                    switch apError {
                     case .invalidUrl:
                         alertItem = AlertContext.invalidUrl
-                        
                     case .invalidResponse:
                         alertItem = AlertContext.invalidResponse
-                        
                     case .invalidData:
                         alertItem = AlertContext.invalidData
-                        
                     case .unableToComplete:
                         alertItem = AlertContext.unableToComplete
                     }
+                } else {
+                    alertItem = AlertContext.invalidResponse
                 }
+                
+                isLoading = false
             }
-            
         }
+        
+//        NetworkManager.shared.getAppetizers { result in
+//            DispatchQueue.main.async { [weak self] in
+//                guard let self = self else {
+//                    return
+//                }
+//
+//                isLoading = false
+//
+//                switch result {
+//                case .success(let appetizers):
+//                    self.appetizers = appetizers
+//
+//                case .failure(let error):
+//                    switch error {
+//                    case .invalidUrl:
+//                        alertItem = AlertContext.invalidUrl
+//
+//                    case .invalidResponse:
+//                        alertItem = AlertContext.invalidResponse
+//
+//                    case .invalidData:
+//                        alertItem = AlertContext.invalidData
+//
+//                    case .unableToComplete:
+//                        alertItem = AlertContext.unableToComplete
+//                    }
+//                }
+//            }
+//
+//        }
     }
 }
